459cb604c9a70f2464292954e13c323ce66277f5
Evgenii <websys.forever@gmail.com>
2022-10-28 13:49:33 +0500

    Added genDiff() and asciinema

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/websys-forever/php-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/websys-forever/php-project-lvl2/actions)
+[![Actions Status](https://github.com/websysforever/php-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/websysforever/php-project-lvl2/actions)
+
+**just training project "Parameters diff" (without OOP)**
+
+Install project:
+
+      composer global require websysforever/php-project-lvl2  
+
+Run diff generator:
+
+      bin/gendiff file1.json file2.json
+
+Example:
+
+[![asciicast](https://asciinema.org/a/QOhDS5fKt5OWH8DmwPHd7AKNI.svg)](https://asciinema.org/a/QOhDS5fKt5OWH8DmwPHd7AKNI)    
---
+++ src/functions.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebsysForever\Differ;
+
+/**
+ * @throws \Exception
+ */
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $file1 = readJsonFile($pathToFile1);
+    $file2 = readJsonFile($pathToFile2);
+    $params1 = json_decode($file1, true);
+    $params2 = json_decode($file2, true);
+
+    $allParamsNames = array_merge(array_keys($params1), array_keys($params2));
+    $allParamsNames = array_unique($allParamsNames);
+    sort($allParamsNames);
+
+    $result = [];
+    foreach ($allParamsNames as $name) {
+        if (array_key_exists($name, $params1)
+            && array_key_exists($name, $params2)
+            && $params1[$name] === $params2[$name]
+        ) {
+            $result[$name] = $params1[$name];
+
+            continue;
+        }
+
+        if (array_key_exists($name, $params1)) {
+            $result["-{$name}"] = $params1[$name];
+        }
+
+        if (array_key_exists($name, $params2)) {
+            $result["+{$name}"] = $params2[$name];
+        }
+    }
+
+    return json_encode($result);
+}
+
+/**
+ * @throws \Exception
+ */
+function readJsonFile(string $path): string
+{
+    if (empty($path)) {
+        throw new \Exception("File path not passed");
+    }
+
+    $realPath = realpath($path);
+
+    if (false === $realPath) {
+        throw new \Exception("File not found");
+    }
+
+    if (!is_file($realPath)) {
+        throw new \Exception("The passed path points to a directory");
+    }
+
+    if (!is_readable($realPath)) {
+        throw new \Exception("File isn't readable");
+    }
+
+    $content = file_get_contents($path);
+
+    if (!$content) {
+        throw new \Exception("File reading error");
+    }
+
+    return $content;
+}
