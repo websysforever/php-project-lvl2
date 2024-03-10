371163b7b971d5107a42c896f477faa1d760db8e
Evgenii <websys.forever@gmail.com>
2024-03-10 12:42:55 +0500

    + plain diff formatter

---
+++ src/Formatters/Plain.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebsysForever\Differ\Formatters\Plain;
+
+/**
+ * @throws \Exception
+ */
+function render(array $diffItems): string
+{
+    /** @TODO plain format */
+    return 'plain format';
+}
